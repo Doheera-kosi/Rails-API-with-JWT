f6883ba20d0d686769a8b70258ecee85d30df78c
Doheera-kosi <kupourevans99@gmail.com>
2023-04-25 10:38:51 +0000

    modify cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "example.com"
 
-    resource "*",
+    resource "example.com",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
